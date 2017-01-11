c93399fd23e9f14035cac2c14ae4fae68b2cfd7a
Bastien Labelle <hello@bastienlabelle.fr>
2017-01-11 21:46:19 +0100

    examples

--- examples/index.php
+++ examples/index.php
@@ -1,1 +1,19 @@
 <?php
+include __DIR__ . '/../vendor/autoload.php';
+
+use Cruiser\Cruiser;
+
+$whoops = new \Whoops\Run;
+$errorPage = new \Whoops\Handler\PrettyPageHandler;
+$whoops->pushHandler($errorPage);
+$whoops->register();
+
+$app = new Cruiser();
+$app->get('/',function() use ($app) {
+    return "Hello";
+});
+$app->get('/user/{id}', function($id) use ($app) {
+    gotest();
+    return "User ".$id;
+});
+$app->run();
